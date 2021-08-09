8d9d3354efb2839ac9782668df8e30032e98f8e0
caiodelucena <caiodelucena6@gmail.com>
2021-08-09 14:30:27 -0300

    add edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,32 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+  def edit 
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @create_article = Article.new(params.require(:article).permit(:title, :description))
+    
+    if @create_article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@create_article) # or use only @create_article (shortcut)
+    else
+      render "new"
+    end  
+  end
+
+  def update 
+    @update_article = Article.find(params[:id])
+    if @update_article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @update_article
+    else
+      render "edit"
+    end
+  end
 end
