ca03e8534e094f1e3688b7c42417caaefdb73fcd
caiodelucena <caiodelucena6@gmail.com>
2021-08-19 09:44:36 -0300

    bootstrap configured for rails 5 and started the front end part

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
+gem 'bootstrap', '~> 4.4.1'
+gem 'jquery-rails'
+
 gem 'pry-byebug'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -9,31 +9,43 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
   end
 
   def edit 
-    @article = Article.find(params[:id])
   end
 
   def create
-    @create_article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     
-    if @create_article.save
+    if @article.save
       flash[:notice] = "Article was created successfully"
-      redirect_to article_path(@create_article) # or use only @create_article (shortcut)
+      redirect_to article_path(@article) # or use only @create_article (shortcut)
     else
       render "new"
     end  
   end
 
   def update 
-    @update_article = Article.find(params[:id])
-    if @update_article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
-      redirect_to @update_article
+      redirect_to @article
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+  @article = Article.find(params[:id])
+  end
+
+  def article_params 
+    params.require(:article).permit(:title, :description)
   end
 end
