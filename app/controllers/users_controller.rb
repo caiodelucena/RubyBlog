a0b97871c7c0ea1b1b1cec77741933bbf49c5f79
Caio Lucena <caiodelucena6@gmail.com>
2021-09-23 20:50:38 -0300

    Modify layout and redirect link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else 
       render "new"
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "User was updated successfully"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "edit"
     end
